b3b83d07feee8d8a8b6ce19b824c5cc35948cf83
risaldanish <risaldanish@gmail.com>
2026-05-20 10:41:44 +0530

    Time: 2 ms (40.71%) | Memory: 56 MB (30.00%) - LeetSync

---
+++ 2752-sum-multiples/sum-multiples.js
@@ -0,0 +1,11 @@
+
+
+var sumOfMultiples = function(n) {
+    sum = 0;
+    for(let i=1 ; i<=n ; i++){
+        if(i%3===0 || i%5 === 0 || i%7===0){
+            sum+=i
+        }
+    }return sum
+};
+sumOfMultiples(7)
